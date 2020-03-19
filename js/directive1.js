f4c5592c9adeeab79b05b239df7ffcfa42a79153
Eleni Margariti <ekamarga@gmail.com>
2020-03-19 19:03:56 +0000

    Update directive1.js

--- js/directive1.js
+++ js/directive1.js
@@ -132,7 +132,6 @@ function zoom(classNames, settings) {
     });
 
 
-
     /* EVENT - touchstart - document ********************************************/
     /****************************************************************************/
     document.addEventListener("touchstart", function() {
@@ -151,7 +150,131 @@ function zoom(classNames, settings) {
     /****************************************************************************/
     document.addEventListener("touchend", touchEnd);
 
+    
+    /* @-<mouseDown *************************************************************/
+    /****************************************************************************/
+    function mouseDown(e) {
+        e.preventDefault();
 
+        if (touchable === true || e.which !== 1) {
+            return false;
+        }
+
+        /* Initialize selectors */
+        $container = this;
+        $element = this.children[0];
+
+        /* Initialize helpers */
+        initialPointerOffsetX = e.clientX;
+        initialPointerOffsetY = e.clientY;
+
+        /* Doubleclick */
+        if (doubleClickMonitor[0] === null) {
+            doubleClickMonitor[0] = e.target;
+            doubleClickMonitor[1] = initialPointerOffsetX;
+            doubleClickMonitor[2] = initialPointerOffsetY;
+
+            setTimeout(function() {
+                doubleClickMonitor = [null];
+            }, 300);
+        } else if (doubleClickMonitor[0] === e.target && mousemoveCount <= 5 && isWithinRange(initialPointerOffsetX, doubleClickMonitor[1] - 10, doubleClickMonitor[1] + 10) === true && isWithinRange(initialPointerOffsetY, doubleClickMonitor[2] - 10, doubleClickMonitor[2] + 10) === true) {
+            addClass($element, _transition);
+
+            if (hasClass($container, _active) === true) {
+                /* Set attributes */
+                $element.setAttribute(_dataScale, 1);
+                $element.setAttribute(_dataTranslateX, 0);
+                $element.setAttribute(_dataTranslateY, 0);
+
+                removeClass($container, _active);
+
+                /* @->moveScaleElement */
+                moveScaleElement($element, 0, 0, 1);
+            } else {
+                /* Set attributes */
+                $element.setAttribute(_dataScale, C_scaleDefault);
+                $element.setAttribute(_dataTranslateX, 0);
+                $element.setAttribute(_dataTranslateY, 0);
+
+                addClass($container, _active);
+
+                /* @->moveScaleElement */
+                moveScaleElement($element, 0, 0, C_scaleDefault);
+            }
+
+            setTimeout(function()
+            {
+                removeClass($element, _transition);
+            }, 200);
+
+            doubleClickMonitor = [null];
+            return false;
+        }
+
+        /* Initialize helpers */
+        offset = $container.getBoundingClientRect();
+        containerOffsetX = offset.left;
+        containerOffsetY = offset.top;
+        containerHeight = $container.clientHeight;
+        containerWidth = $container.clientWidth
+        elementHeight = $element.clientHeight;
+        elementWidth = $element.clientWidth;
+        initialOffsetX = parseFloat($element.getAttribute(_dataTranslateX));
+        initialOffsetY = parseFloat($element.getAttribute(_dataTranslateY));
+        initialScale = minMax(parseFloat($element.getAttribute(_dataScale)), C_scaleMin, C_scaleMax);
+
+        mousemoveCount = 0;
+
+        /* Set capture */
+        capture = true;
+    }
+
+    /* @-<mouseMove *************************************************************/
+    /****************************************************************************/
+    function mouseMove(e) {
+        if (touchable === true || capture === false) {
+            return false;
+        }
+
+        /* Initialize helpers */
+        pointerOffsetX = e.clientX;
+        pointerOffsetY = e.clientY;
+        targetScale = initialScale;
+        limitOffsetX = ((elementWidth * targetScale) - containerWidth) / 2;
+        limitOffsetY = ((elementHeight * targetScale) - containerHeight) / 2;
+        targetOffsetX = (elementWidth * targetScale) <= containerWidth ? 0 : minMax(pointerOffsetX - (initialPointerOffsetX - initialOffsetX), limitOffsetX * (-1), limitOffsetX);
+        targetOffsetY = (elementHeight * targetScale) <= containerHeight ? 0 : minMax(pointerOffsetY - (initialPointerOffsetY - initialOffsetY), limitOffsetY * (-1), limitOffsetY);
+        mousemoveCount++;
+
+        if (Math.abs(targetOffsetX) === Math.abs(limitOffsetX)) {
+            initialOffsetX = targetOffsetX;
+            initialPointerOffsetX = pointerOffsetX;
+        }
+
+        if (Math.abs(targetOffsetY) === Math.abs(limitOffsetY)) {
+            initialOffsetY = targetOffsetY;
+            initialPointerOffsetY = pointerOffsetY;
+        }
+
+        /* Set attributes */
+        $element.setAttribute(_dataScale, targetScale);
+        $element.setAttribute(_dataTranslateX, targetOffsetX);
+        $element.setAttribute(_dataTranslateY, targetOffsetY);
+
+        /* @->moveScaleElement */
+        moveScaleElement($element, targetOffsetX + "px", targetOffsetY + "px", targetScale);
+    }
+
+    /* @-<mouseUp ***************************************************************/
+    /****************************************************************************/
+    function mouseUp() {
+        if (touchable === true || capture === false) {
+            return false;
+        }
+
+        /* Unset capture */
+        capture = false;
+    }
 
     /* @-<touchStart ************************************************************/
     /****************************************************************************/
@@ -344,7 +467,64 @@ function zoom(classNames, settings) {
         }
     }
 
-    
+    /* @-<wheel *****************************************************************/
+    /****************************************************************************/
+    function wheel(e) {
+        /* Initialize selectors */
+        $container = this;
+        $element = this.children[0];
+
+        /* Initialize helpers */
+        offset = $container.getBoundingClientRect();
+        containerHeight = $container.clientHeight;
+        containerWidth = $container.clientWidth;
+        elementHeight = $element.clientHeight;
+        elementWidth = $element.clientWidth;
+        containerOffsetX = offset.left;
+        containerOffsetY = offset.top;
+        initialScale = minMax(parseFloat($element.getAttribute(_dataScale), C_scaleMin, C_scaleMax));
+        initialOffsetX = parseFloat($element.getAttribute(_dataTranslateX));
+        initialOffsetY = parseFloat($element.getAttribute(_dataTranslateY));
+        pointerOffsetX = e.clientX;
+        pointerOffsetY = e.clientY;
+        scaleDirection = e.deltaY < 0 ? 1 : -1;
+        scaleDifference = C_scaleDifference * scaleDirection;
+        targetScale = initialScale + scaleDifference;
+
+        /* Prevent scale overflow */
+        if (targetScale < C_scaleMin || targetScale > C_scaleMax) {
+            return false;
+        }
+
+        /* Set offset limits */
+        limitOffsetX = ((elementWidth * targetScale) - containerWidth) / 2;
+        limitOffsetY = ((elementHeight * targetScale) - containerHeight) / 2;
+
+        if (targetScale <= 1) {
+            targetOffsetX = 0;
+            targetOffsetY = 0;
+        } else {
+            /* Set target offsets */
+            targetOffsetX = (elementWidth * targetScale) <= containerWidth ? 0 : minMax(initialOffsetX - ((((((pointerOffsetX - containerOffsetX) - (containerWidth / 2)) - initialOffsetX) / (targetScale - scaleDifference))) * scaleDifference), limitOffsetX * (-1), limitOffsetX);
+            targetOffsetY = (elementHeight * targetScale) <= containerHeight ? 0 : minMax(initialOffsetY - ((((((pointerOffsetY - containerOffsetY) - (containerHeight / 2)) - initialOffsetY) / (targetScale - scaleDifference))) * scaleDifference), limitOffsetY * (-1), limitOffsetY);
+        }
+
+        if (targetScale > 1) {
+            addClass($container, _active);
+        } else {
+            removeClass($container, _active);
+        }
+
+        /* Set attributes */
+        $element.setAttribute(_dataScale, targetScale);
+        $element.setAttribute(_dataTranslateX, targetOffsetX);
+        $element.setAttribute(_dataTranslateY, targetOffsetY);
+
+        /* @->moveScaleElement */
+        moveScaleElement($element, targetOffsetX + "px", targetOffsetY + "px", targetScale);
+    }
+}
+
 /* Library ********************************************************************/
 /******************************************************************************/
 
