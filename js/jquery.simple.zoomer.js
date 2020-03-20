abb28b9461d389b4560f4088c5057a85be1bc908
Eleni Margariti <ekamarga@gmail.com>
2020-03-20 14:53:11 +0000

    Update jquery.simple.zoomer.js

--- js/jquery.simple.zoomer.js
+++ js/jquery.simple.zoomer.js
@@ -40,7 +40,7 @@ $(slider).on('input', function(){
     zoomer = zoom/10;
     $(objectToZoom).css({
 /*Images will zoom in and out with the CSS3 transformation*/
-     'transform' : 'scale('+zoomer+') translate(-100%, -100%)' ,
+     'transform' : 'scale('+zoomer+') translate(0%, 0%)' ,
 /*The origin where from zooming transformation starts */
      'transformOrigin' : setting.zoomingOrigin,
  });
